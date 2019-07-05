89c7d880f20dd48c517e0f0eb6d8ea396b73abb8
tomrosier <tom.rosier@digital.cabinet-office.gov.uk>
2019-07-05 14:30:46 +0100

    Fixed IOS font issue, roboto is now found

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Text, StatusBar, View, StyleSheet } from 'react-native';
+import * as Font from 'expo-font'
 
 import { COLOR, ThemeContext, getTheme } from 'react-native-material-ui';
 import AppView from './views/AppView';
@@ -10,6 +11,10 @@ const uiTheme = {
     accentColor: COLOR.pink500,
   },
 };
+
+Font.loadAsync({
+  roboto: require('./assets/fonts/Roboto-Regular.ttf'),
+});
 
 export default function App() {
   return (
